68ac8f2e61a42f59cf8419e37d3395e32121447d
Raphael Tango05 <raphael.tango05@gmail.com>
2022-11-13 21:37:48 +0100

    finish challenge

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double myVariable = 20.00d;
+        double myVariable2 = 80.00d;
+        double result = ((myVariable+myVariable2)*100d)%40.00d;
+        boolean resultBool = result == 0.00 ? true : false;
+        System.out.println(resultBool);
+
+        if(!resultBool) {
+            System.out.println("Got some remainder");
+        }
+
     }
 }
